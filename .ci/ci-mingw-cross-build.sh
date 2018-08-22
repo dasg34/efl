eec015fd697c27ea37f2fd1cfc1de6f221c834e8
Stefan Schmidt <s.schmidt@samsung.com>
2018-08-22 14:51:12 +0200

    ci: mingw build target to cross compile EFL for Windows
    We are using the EFL windows package installer (ewpi) from Vincent Torri
    here (thanks!) to setup all the needed cross compiled dependencies for
    EFL.

---
+++ .ci/ci-mingw-cross-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PREFIX=/root/EFL/ewpi_64
+export HOST=x86_64-w64-mingw32
+export DISABLE_STATIC="--disable-static"
+export CFLAGS="-pipe -g3 -ggdb3 -Og"
+export CXXFLAGS="-pipe -g3 -ggdb3 -Og"
+export CPPFLAGS="-I$PREFIX/include -DECORE_WIN32_WIP_POZEFLKSD"
+export LDFLAGS="-L$PREFIX/lib/"
+#export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig/
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/
+
+autoreconf -vif
+./configure --prefix=$PREFIX --host=$HOST --with-eolian-gen=/usr/bin/eolian_gen --disable-static --with-tests=regular --with-crypto=openssl --disable-gstreamer1 --disable-gstreamer --disable-libmount --disable-valgrind --disable-avahi --disable-spectre --disable-libraw --disable-librsvg --disable-pulseaudio --disable-cxx-bindings --disable-image-loader-jp2k
+make
