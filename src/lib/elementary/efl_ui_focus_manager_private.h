53724cd392986b66344a673f36a347be28f50373
Yeongjong Lee <yj34.lee@samsung.com>
2018-09-18 16:38:03 +0900

    efl_ui_focus_manager: implement manager_focus, unfocus

--- src/lib/elementary/efl_ui_focus_manager_private.h
+++ src/lib/elementary/efl_ui_focus_manager_private.h
@@ -7,10 +7,12 @@ typedef struct _Efl_Ui_Focus_Manager_Data Efl_Ui_Focus_Manager_Data;
 struct _Efl_Ui_Focus_Manager_Data
 {
    Eina_List *focused_list;
+   Eina_Hash *focus_stack;
    Efl_Canvas_Object *current_focus;
 };
 
-Eina_Bool _efl_ui_focus_manager_focus(Efl_Ui_Focus_Manager *obj, Efl_Ui_Focusable *focus_obj);
-void _efl_ui_focus_manager_unfocus(Efl_Ui_Focus_Manager *obj, Efl_Ui_Focusable *focus_obj);
+void _efl_ui_focus_manager_handle(Efl_Ui_Focusable *obj);
+Eina_Bool _efl_ui_focus_manager_focus(Efl_Ui_Focusable *obj);
+Eina_Bool _efl_ui_focus_manager_unfocus(Efl_Ui_Focusable *obj);
 
 #endif //EFL_UI_FOCUS_MANAGER_H
