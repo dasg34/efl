9a63b1792b036785f441b5d8ecbf47c33706cbeb
Yeongjong Lee <yj34.lee@samsung.com>
2018-09-19 14:19:16 +0900

    efl_ui_focus_manager: implement current, previous_focus_get

--- src/lib/elementary/efl_ui_focus_manager_private.h
+++ src/lib/elementary/efl_ui_focus_manager_private.h
@@ -7,6 +7,9 @@ typedef struct _Efl_Ui_Focus_Manager_Data Efl_Ui_Focus_Manager_Data;
 struct _Efl_Ui_Focus_Manager_Data
 {
    Eina_List *focused_list;
+   Efl_Canvas_Object *current_focus;
+   Efl_Canvas_Object *prev_focus;
+   Efl_Ui_Focus_Direction prev_dir;
 };
 
 Eina_Bool _efl_ui_focus_manager_focus(Efl_Ui_Focus_Manager *obj, Efl_Ui_Focusable *focus_obj);
