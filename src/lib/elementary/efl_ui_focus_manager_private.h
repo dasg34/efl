42094285ae01b7165d1dd1208fa9ef412c22f5cc
Yeongjong Lee <yj34.lee@samsung.com>
2018-09-20 16:50:30 +0900

    efl_ui_win: migrate to new focus manager

--- src/lib/elementary/efl_ui_focus_manager_private.h
+++ src/lib/elementary/efl_ui_focus_manager_private.h
@@ -8,11 +8,14 @@ struct _Efl_Ui_Focus_Manager_Data
 {
    Eina_List *focused_list;
    Eina_Hash *focus_stack;
+   Efl_Canvas_Object *last_focus;
    Efl_Canvas_Object *current_focus;
 };
 
 void _efl_ui_focus_manager_handle(Efl_Ui_Focusable *obj);
+void _efl_ui_focus_manager_last_focus_restore(Efl_Ui_Focusable *obj);
 Eina_Bool _efl_ui_focus_manager_focus(Efl_Ui_Focusable *obj);
 Eina_Bool _efl_ui_focus_manager_unfocus(Efl_Ui_Focusable *obj);
+void _efl_ui_focus_manager_active_manager_set(Efl_Ui_Focus_Manager *manager);
 
 #endif //EFL_UI_FOCUS_MANAGER_H
